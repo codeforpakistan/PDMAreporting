3afa36b2b54f2657dfe552f97f1ce3adac511621
Muhammad Zeeshan <engr.zeeshan13@gmail.com>
2015-09-30 14:18:33 +0500

    Basic config changes
    setting of template, default controller, config, autoloads and htaccess

---
+++ application/views/template/footer.php
@@ -0,0 +1,7 @@
+		<!--js files-->
+		<script src="js/jquery-2.1.4.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/login_animation.js"></script>
+		<script src="js/angular.min.js"></script>
+	</body>
+</html>
---
+++ application/views/template/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/login.css"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/anicollection.css"/>
+    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"> -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.js"></script>
+    <![endif]-->
+    <style>
+        /*body{*/
+            /*background-color: #989898;*/
+            /*height: 100px;*/
+        /*}*/
+    </style>
+</head>
+<body>
