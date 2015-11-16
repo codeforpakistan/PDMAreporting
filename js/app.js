7657e5e3027b080b05fcd39c47eda7d4693d2b28
Muhammad Zeeshan <engr.zeeshan13@gmail.com>
2015-11-16 20:32:10 +0500

    added "get by id" in app

--- js/app.js
+++ js/app.js
@@ -37,6 +37,11 @@ app.controller('DeadInjuredCtrl', function($scope, $http) {
                 $scope.message = "Data Inserted";
               });
         }
+        $scope.getbyid = function(id){
+          $http.get('/deadinjured/get_by_id' + id).success(function(data, status, headers, config){
+            $scope.row = data;
+          });
+        }
 
         //get dead data
         $http.get(baseURL+'deadinjured/get_all').success(function(data, status, headers, config){    
@@ -60,6 +65,12 @@ app.controller('HouseDamageCtrl', function($scope, $http) {
               });
         }
 
+        $scope.getbyid = function(id){
+          $http.get('/house/get_by_id' + id).success(function(data, status, headers, config){
+            $scope.row = data;
+          });
+        }
+
         //get houses data
         $http.get(baseURL+'house/get_all').success(function(data, status, headers, config){      
           $scope.table_info = data;
@@ -74,18 +85,24 @@ app.controller('CattleCtrl', function($scope, $http) {
           $scope.budget = data;
         });
 
-  $scope.addCattle = function(data){
-    //var mydata = JSON.stringify(data);
-    $scope.message = "Data is being inserted...";
-        $http.post('http://localhost/PDMAreporting/cattle/add',data).success(function(data, status, headers, config){          
-          $scope.message = "Data Inserted";
-        });
-    /*
-        $http.get('http://localhost/PDMAreporting/cattle/add2/'+1).success(function(data, status, headers, config){
-          console.log(data);
-        });
-    */
-  }
+        $scope.addCattle = function(data){
+          //var mydata = JSON.stringify(data);
+          $scope.message = "Data is being inserted...";
+              $http.post('http://localhost/PDMAreporting/cattle/add',data).success(function(data, status, headers, config){          
+                $scope.message = "Data Inserted";
+              });
+          /*
+              $http.get('http://localhost/PDMAreporting/cattle/add2/'+1).success(function(data, status, headers, config){
+                console.log(data);
+              });
+          */
+        }
+
+        $scope.getbyid = function(id){
+          $http.get('/cattle/get_by_id' + id).success(function(data, status, headers, config){
+            $scope.row = data;
+          });
+        }
 
     //get cattle data
     $http.get(baseURL+'cattle/get_all').success(function(data, status, headers, config){
